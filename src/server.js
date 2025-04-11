dab88c037fc1099b181492d5e397af94e6645e4a
jpedroccruz <joaopedroccruz.dev@gmail.com>
2025-04-10 23:21:35 -0300

    feat: create express server and connection with mysql

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express, { json } from 'express'
+import getUserData from './routes/getUserData.js'
+import updateData from './routes/continueScreening.js'
+import continueScreening from './routes/continueScreening.js'
+
+const app = express()
+
+app.use(json())
+
+app.get('/', (_, res) => {
+  res.status(200).send({ menssage: "Hello World" })
+}) 
+
+app.get('/user/:susCode', getUserData)
+
+app.listen(3333, () => console.log("Server running on port 3333."))
